294c06a1f417c915c94184509aabd38fe4b1d77f
JakubPrwsz <152688116+JakubPrwsz@users.noreply.github.com>
2023-12-02 11:30:41 +0100

    Add files via upload

--- rep_zad_1.py
+++ rep_zad_1.py
@@ -1,7 +1,24 @@
 '''
-Przykładowy kod python
-
-
+kalkulator prosty
 '''
+def main():
+    a = int(input("podaj pierwszą liczbę "))
+
+    b = int(input("podaj drugą liczbę "))
+
+    print("1: dodawanie\n2: odejmowanie\n3: mnożenie\n4: dzielenie")
+
+    dzialanie = input("podaj numer dzialania ktore chcesz wykonac: ")
+
+    match dzialanie:
+        case "1":
+            print(a + b)
+        case "2":
+            print(a - b)
+        case "3":
+            print(a * b)
+        case "4":
+            print(a / b)
 
 
+main()
